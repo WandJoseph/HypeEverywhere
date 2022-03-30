7d58709b71b81bc13598818be4c26698426afd5f
Wand Joseph <wand27dev@gmail.com>
2022-03-30 18:05:11 -0300

    Unexistent import removed

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,6 @@ import { CharacterAttributeModule } from './character-attribute/character-attrib
 import { CharacterDiscordModule } from './character-discord/character.discord.module';
 import { CharacterTechniqueModule } from './character-technique/character-technique.module';
 import { CharacterModule } from './character/user.module';
-import { ConditionsModule } from './conditions/conditions.module';
 import { UserCharacterModule } from './user-character/user-character.module';
 import { UserModule } from './user/user.module';
 
